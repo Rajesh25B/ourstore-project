c147bbd2af285722804f38eaa0f77b0595f92994
Rajesh B <55087178+Rajesh25B@users.noreply.github.com>
2023-01-13 19:32:37 +0530

    Update orderActions.js

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -48,7 +48,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
         }
 
         const { data } = await axios.post(
-            `/api/orders/add/`,
+            `https://ashrayshiva.pythonanywhere.com/api/orders/add/`,
             order,
             config
         )
@@ -96,7 +96,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
         }
 
         const { data } = await axios.get(
-            `/api/orders/${id}/`,
+            `https://ashrayshiva.pythonanywhere.com/api/orders/${id}/`,
             config
         )
 
@@ -134,7 +134,7 @@ export const payOrder = (id, paymentResult) => async (dispatch, getState) => {
         }
 
         const { data } = await axios.put(
-            `/api/orders/${id}/pay/`,
+            `https://ashrayshiva.pythonanywhere.com/api/orders/${id}/pay/`,
             paymentResult,
             config
         )
@@ -173,7 +173,7 @@ export const ListMyOrders = (id) => async (dispatch, getState) => {
         }
 
         const { data } = await axios.get(
-            `/api/orders/myorders/`,
+            `https://ashrayshiva.pythonanywhere.com/api/orders/myorders/`,
             config
         )
 
@@ -211,7 +211,7 @@ export const listOrders = () => async (dispatch, getState) => {
         }
 
         const { data } = await axios.get(
-            `/api/orders/`,
+            `https://ashrayshiva.pythonanywhere.com/api/orders/`,
             config
         )
 
@@ -249,7 +249,7 @@ export const deliverOrder = (order) => async (dispatch, getState) => {
         }
 
         const { data } = await axios.put(
-            `/api/orders/${order._id}/deliver/`,
+            `https://ashrayshiva.pythonanywhere.com/api/orders/${order._id}/deliver/`,
             {},
             config
         )
