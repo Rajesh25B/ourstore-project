2d95037891597eabfc853e15d9dc726f9eb24a58
Rajesh B <55087178+Rajesh25B@users.noreply.github.com>
2023-01-14 16:18:39 +0530

    Update ProductCarousel.js

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { Carousel, Image } from 'react-bootstrap'
 import Loader from './Loader'
 import Message from './Message'
+import products from './products;
 import { listTopProducts } from '../actions/productActions'
 
 function ProductCarousel() {
