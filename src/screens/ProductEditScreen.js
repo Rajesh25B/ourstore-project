190e041e4866601680094f1fa22ea82d1ec3808e
Rajesh B <55087178+Rajesh25B@users.noreply.github.com>
2023-01-25 21:20:02 +0530

    Update ProductEditScreen.js

--- src/screens/ProductEditScreen.js
+++ src/screens/ProductEditScreen.js
@@ -34,7 +34,7 @@ function ProductEditScreen({ match, history }) {
 
         if (successUpdate) {
             dispatch({ type: PRODUCT_UPDATE_RESET })
-            history.push('/admin/productlist')
+            history.push('https://ashrayshiva.pythonanywhere.com/admin/productlist')
         } else { 
         if (!product.name || product._id !== Number(productId)) {
             dispatch(listProductDetails(productId))
@@ -81,7 +81,7 @@ function ProductEditScreen({ match, history }) {
                 }
             }
 
-            const { data } = await axios.post('/api/products/upload/', formData, config)
+            const { data } = await axios.post('https://ashrayshiva.pythonanywhere.com/api/products/upload/', formData, config)
 
 
             setImage(data)
@@ -94,7 +94,7 @@ function ProductEditScreen({ match, history }) {
         
     return (
         <div>
-            <Link to='/admin/productlist'>
+            <Link to='https://ashrayshiva.pythonanywhere.com/admin/productlist'>
                 Go Back
             </Link>
 
